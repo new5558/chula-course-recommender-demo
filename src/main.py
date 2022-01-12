a9255dbf6bc1260ad275dfc14a481620624d5d64
new5558 <new17353@gmail.com>
2022-01-13 06:49:08 +0700

    feat: add skeleton template

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import streamlit.components.v1 as components
-from transformers import TextClassificationPipeline, AutoModelForSequenceClassification, AutoTokenizer, PreTrainedTokenizer
 from thai2transformers.preprocess import process_transformers
-from tokenizers import Tokenizer, AddedToken
-import shap
 import requests
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
 import os
+import typing
+from typing import List
 
 # model_path = './model'
 # txt = ""
@@ -32,11 +34,35 @@ import os
 #             return label
 #     return mapper
 
-# @st.cache()
-# def download_faculty_map():
-#     r = requests.get('https://raw.githubusercontent.com/DSCChula/chula-util/main/src/faculties.json')
-#     label_map = r.json()
-#     return label_map
+@st.cache()
+def load_features_array() -> npt.NDArray[np.float64]:
+    dict_data_0 = np.load('./course_features/features_array_0.npz')['arr_0']
+    dict_data_1 = np.load('./course_features/features_array_1.npz')['arr_0']
+    dict_data_2 = np.load('./course_features/features_array_2.npz')['arr_0']
+    dict_data_3 = np.load('./course_features/features_array_3.npz')['arr_0']
+
+    return np.concatenate((dict_data_0, dict_data_1, dict_data_2, dict_data_3), axis=0)
+
+@st.cache()
+def load_texts_array() -> List[str]:
+    texts = pd.read_csv('./course_features/texts_total.csv', index_col=0)['0']
+    return texts.values.tolist()
+
+@st.cache()
+def load_course_dataframe() -> pd.DataFrame:
+    courses_df = pd.read_csv('./course_features/courses_recommender_demo.csv', index_col=0)
+    return courses_df[['course_no', 'course_name_thai', 'description_thai']]
+
+@st.cache()
+def generate_courses_key_index(all_courses_key: List[str]) \
+     -> typing.TypedDict('Key index', key=str, index=int):
+    dict = {}
+    for i in range(len(all_courses_key)):
+        key = all_courses_key[i].strip()
+        dict[key] = i
+
+    return dict
+
 
 # @st.cache(allow_output_mutation=True, suppress_st_warning=True, max_entries=1, hash_funcs={Tokenizer: hash_tokenizer, AddedToken: hash_tokenizer})
 # def load_pipeline():
@@ -51,17 +77,50 @@ import os
 #     return pipeline, model.config
 
 
+
 def main():
-    title = "Chula Course Faculty prediction demo"
+    if 'button_sent' not in st.session_state:
+        st.session_state['button_sent'] = False
+    title = "Chula Course Recommendation demo"
     
-    # st.set_page_config(page_title=title, page_icon="🎓", layout="wide")
+    st.set_page_config(page_title=title, page_icon="🎓", layout="wide")
     # _, center_column, __ = st.columns((1, 2, 1))
     # center_column.write("![github](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)  [Project's repo](https://github.com/new5558/chula-course-faculty-prediction-demo)")
-    # center_column.title(title)
+    st.title(title)
+
+    all_courses_key = load_texts_array()
+    all_courses_features = load_features_array()
+    courses_df = load_course_dataframe()
+
+    courses_key_index = generate_courses_key_index(all_courses_key)
+    # st.write(courses_key_index)
+
+    form = st.form(key='search')
+    query = form.text_input('Course search query', '')
+    submit = form.form_submit_button(label='Submit')
+
+    if submit:
+        st.session_state['button_sent'] = True
+
+    
+    if st.session_state['button_sent']:
+        result = courses_df[courses_df.apply(lambda r: r.str.contains(query, case=False).any(), axis=1)] 
+
+        option = st.selectbox('Search result', result['course_no'] + ": " + result['course_name_thai'], key=result['course_name_thai'])
+
+        st.write('You selected:', option)
+        course_no = option.split(':')[0]
+        st.write(course_no)
+
+        course_description = result[result['course_no'] == course_no]['description_thai'].iloc[0]
+        st.write(course_description)
+        
+        course_description_processed = process_transformers(course_description).strip()
+        st.write(courses_key_index[course_description_processed])
 
 
-    # if 'button_sent' not in st.session_state:
-    #     st.session_state['button_sent'] = False
+
+
     
     # pipeline, model_config = load_pipeline()
     # label_map = download_faculty_map()
@@ -70,7 +129,6 @@ def main():
     # if center_column.button(label="Predict Faculty"):
     #     st.session_state['button_sent'] = True
 
-    # if st.session_state['button_sent']:
     #     processed_text = process_transformers(txt)
 
     #     prediction = pipeline(processed_text)[0]
