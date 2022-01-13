8f6500d1694608a1dcb215de09036f6201ef5911
new5558 <new17353@gmail.com>
2022-01-13 20:05:50 +0700

    feat: add functional recommendaiton demo

--- src/main.py
+++ src/main.py
@@ -8,31 +8,22 @@ import pandas as pd
 import os
 import typing
 from typing import List
+import sentencepiece as spm
 
-# model_path = './model'
-# txt = ""
+max_token_length = 508
 
-# def st_shap(plot, height=None):
-#     shap_html = f"<head>{shap.initjs()}</head><body>{plot}</body>"
-#     components.html(shap_html, height=height)
+def tokenize_transformers(text, sp: spm.SentencePieceProcessor):
+  return sp.encode(process_transformers(text), out_type=str)
 
-# def hash_tokenizer(_):
-#     return model_path
+def replace_long_sentence(text, sp: spm.SentencePieceProcessor):
+    return ''.join(tokenize_transformers(text, sp)[0:max_token_length]).replace('▁', '').strip()
 
-# def get_label_from_text(text, label_map):
-#     for key in label_map.keys():
-#         value = label_map[key]
-#         if value['name_th'] == text:
-#             return key
-#     return text
-
-# def map_name_th(label_map):
-#     def mapper(label):
-#         if label in label_map.keys():
-#             return label_map[label]['name_th']
-#         else:
-#             return label
-#     return mapper
+def create_process_description_thai(sp: spm.SentencePieceProcessor):
+    def process_description_thai(text):
+        course_description_trimmed = replace_long_sentence(text, sp)
+        course_description_processed = process_transformers(course_description_trimmed).strip()
+        return course_description_processed
+    return process_description_thai
 
 @st.cache()
 def load_features_array() -> npt.NDArray[np.float64]:
@@ -41,7 +32,8 @@ def load_features_array() -> npt.NDArray[np.float64]:
     dict_data_2 = np.load('./course_features/features_array_2.npz')['arr_0']
     dict_data_3 = np.load('./course_features/features_array_3.npz')['arr_0']
 
-    return np.concatenate((dict_data_0, dict_data_1, dict_data_2, dict_data_3), axis=0)
+    features = np.concatenate((dict_data_0, dict_data_1, dict_data_2, dict_data_3), axis=0)
+    return features, np.linalg.norm(features, axis = 1) 
 
 @st.cache()
 def load_texts_array() -> List[str]:
@@ -49,9 +41,10 @@ def load_texts_array() -> List[str]:
     return texts.values.tolist()
 
 @st.cache()
-def load_course_dataframe() -> pd.DataFrame:
+def load_course_dataframe(process_description_thai) -> pd.DataFrame:
     courses_df = pd.read_csv('./course_features/courses_recommender_demo.csv', index_col=0)
-    return courses_df[['course_no', 'course_name_thai', 'description_thai']]
+    courses_df['description_thai_key'] = courses_df['description_thai'].apply(process_description_thai)
+    return courses_df[['course_no', 'course_name_thai', 'description_thai', 'description_thai_key']]
 
 @st.cache()
 def generate_courses_key_index(all_courses_key: List[str]) \
@@ -63,6 +56,10 @@ def generate_courses_key_index(all_courses_key: List[str]) \
 
     return dict
 
+@st.cache()
+def load_sentence_pie():
+    sp = spm.SentencePieceProcessor(model_file='model/sentencepiece.bpe.model')
+    return sp
 
 # @st.cache(allow_output_mutation=True, suppress_st_warning=True, max_entries=1, hash_funcs={Tokenizer: hash_tokenizer, AddedToken: hash_tokenizer})
 # def load_pipeline():
@@ -87,13 +84,15 @@ def main():
     # _, center_column, __ = st.columns((1, 2, 1))
     # center_column.write("![github](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)  [Project's repo](https://github.com/new5558/chula-course-faculty-prediction-demo)")
     st.title(title)
+    
+    sp = load_sentence_pie()
 
+    process_description_thai = create_process_description_thai(sp)
     all_courses_key = load_texts_array()
-    all_courses_features = load_features_array()
-    courses_df = load_course_dataframe()
+    all_courses_features, all_courses_features_norm = load_features_array()
+    courses_df = load_course_dataframe(process_description_thai)
 
     courses_key_index = generate_courses_key_index(all_courses_key)
-    # st.write(courses_key_index)
 
     form = st.form(key='search')
     query = form.text_input('Course search query', '')
@@ -110,47 +109,33 @@ def main():
 
         st.write('You selected:', option)
         course_no = option.split(':')[0]
-        st.write(course_no)
 
-        course_description = result[result['course_no'] == course_no]['description_thai'].iloc[0]
+        search_result = result[result['course_no'] == course_no]
+
+        course_description_key = search_result['description_thai_key'].iloc[0]
+        course_description = search_result['description_thai'].iloc[0]
+
         st.write(course_description)
         
-        course_description_processed = process_transformers(course_description).strip()
-        st.write(courses_key_index[course_description_processed])
-
-
-
-
-    
-    # pipeline, model_config = load_pipeline()
-    # label_map = download_faculty_map()
-    # txt = center_column.text_area('Course description thai', 'บทบาทของเทคโนโลยีสารสนเทศในการแก้ปัญหาทางธุรกิจ แนวคิดและพื้นฐานเทคนิคของศาสตร์เทคโนโลยีสารสนเทศ การวางแผน การพัฒนาและการจัดการของระบบประยุกต์สารสนเทศโดยคอมพิวเตอร์')    
-
-    # if center_column.button(label="Predict Faculty"):
-    #     st.session_state['button_sent'] = True
-
-    #     processed_text = process_transformers(txt)
-
-    #     prediction = pipeline(processed_text)[0]
-    #     center_column.json(prediction)
-    #     predicted_label = prediction['label']
-    #     center_column.json(label_map[predicted_label])
-
-    #     predicted_id = model_config.label2id[predicted_label]
+        course_key = courses_key_index[course_description_key]
         
-    #     all_labels = model_config.label2id.keys()
-    #     all_labels_text =  list(map(map_name_th(label_map), all_labels))
-    #     selected_label_text = center_column.selectbox('Target Faculty for SHAP interpretation', all_labels_text, index = predicted_id)
-    #     selected_label = get_label_from_text(selected_label_text, label_map)
-    #     selected_id = model_config.label2id[selected_label]
+        query_vector = all_courses_features[course_key].reshape((1, -1))
+        dot_product = np.dot(query_vector, all_courses_features.T)
+        cosine_similarity = dot_product / (all_courses_features_norm * np.linalg.norm(query_vector, axis = 1) )
+        scores = pd.Series(np.flip(np.sort(cosine_similarity[0])[-10:]), name = 'scores')
 
-    #     explainer = shap.Explainer(pipeline)
-    #     shap_values = None
-    #     with st.spinner(text="Interpreting model with SHAP. we don't have GPU so this will take a 1-2 minutes"):
-    #         shap_values = explainer([processed_text])
-
+        max_course = 10
+        best_indexes = np.flip(np.argsort(cosine_similarity[0])[-max_course:]).tolist()
+        best_keys = [all_courses_key[i] for i in best_indexes]
         
-    #     st_shap(shap.plots.text(shap_values[0, :, selected_id], display = False))
+        best_courses = pd.DataFrame()
+        for key in best_keys:
+            row = courses_df[courses_df['description_thai_key'] == key]
+            best_courses = best_courses.append(row)
+        
+        best_courses = pd.concat([best_courses.reset_index(), scores], axis = 1)
+        best_courses = best_courses.drop(['description_thai_key', 'index'], axis = 1)
+        st.dataframe(best_courses, width=2000)
 
 if __name__ == "__main__":
     main()
