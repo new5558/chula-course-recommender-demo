92d7634c2a409b568754819a7631aea8b2130f0e
new5558 <new17353@gmail.com>
2022-01-13 21:13:38 +0700

    fix: array load naming error

--- src/main.py
+++ src/main.py
@@ -32,14 +32,14 @@ def aggregate_features_array(array1: npt.NDArray[np.float64], array2: npt.NDArra
 @st.cache()
 def load_features_array_1() -> npt.NDArray[np.float64]:
     dict_data_0 = np.load('./course_features/features_train_array_0.npz')['arr_0']
-    dict_data_1 = np.load('./course_features/features_train__1.npz')['arr_0']
+    dict_data_1 = np.load('./course_features/features_train_array_1.npz')['arr_0']
     features = np.concatenate((dict_data_0, dict_data_1), axis=0)
     return features
 
 @st.cache() 
 def load_features_array_2() -> npt.NDArray[np.float64]:
-    dict_data_2 = np.load('./course_features/features_train__2.npz')['arr_0']
-    dict_data_3 = np.load('./course_features/features_train__3.npz')['arr_0']
+    dict_data_2 = np.load('./course_features/features_train_array_2.npz')['arr_0']
+    dict_data_3 = np.load('./course_features/features_train_array_3.npz')['arr_0']
     features = np.concatenate((dict_data_2, dict_data_3), axis=0)
     return features
 
